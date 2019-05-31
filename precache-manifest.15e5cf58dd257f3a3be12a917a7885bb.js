097c6802ee6a51b8ebfdd0c31f327b5958761419
Ayushi <ayushichhabra.itm@gmail.com>
2019-05-31 14:12:56 +0200

    Updates

---
+++ precache-manifest.15e5cf58dd257f3a3be12a917a7885bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe41e12880f0968449e45f63312e079",
+    "url": "/Ayushi1312.github.io/index.html"
+  },
+  {
+    "revision": "18cc61951753f50de9da",
+    "url": "/Ayushi1312.github.io/static/css/main.8c83990f.chunk.css"
+  },
+  {
+    "revision": "c5b0ecde2d034ee50b5b",
+    "url": "/Ayushi1312.github.io/static/js/2.02e28368.chunk.js"
+  },
+  {
+    "revision": "18cc61951753f50de9da",
+    "url": "/Ayushi1312.github.io/static/js/main.21046bf9.chunk.js"
+  },
+  {
+    "revision": "8e550fb2636040bb941f",
+    "url": "/Ayushi1312.github.io/static/js/runtime~main.62d8457d.js"
+  }
+]);
